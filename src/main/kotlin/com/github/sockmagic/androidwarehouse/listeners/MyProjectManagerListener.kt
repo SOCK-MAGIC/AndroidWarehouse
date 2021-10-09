4e7a28176614b1f1b9dc00fbcc0ff88839723f7b
GitHub Action <action@github.com>
2021-10-09 14:19:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/sockmagic/androidwarehouse/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/sockmagic/androidwarehouse/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sockmagic.androidwarehouse.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.sockmagic.androidwarehouse.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/sockmagic/androidwarehouse/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sockmagic.androidwarehouse.services
+
+import com.github.sockmagic.androidwarehouse.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
